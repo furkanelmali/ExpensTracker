7c8cdd2b8d03b9eeaa0979ae70b3cb3f7085a47a
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2024-12-11 01:48:27 +0300

    v1.3
    Some css upgrades, Category add, delete and edit process and user interactions and user interface for this processes are developed

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcd6c6e534307298cc3a3402344e1539d4e07a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1628ed88d1ee236eb55de50e252a44ff9133b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
