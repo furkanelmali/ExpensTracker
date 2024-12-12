f396039d424f9784c2b9e2e262c614510c7accb7
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2024-12-12 16:39:20 +0300

    v2.0
    Dashboarcontroller, sidebar, dashboard uı and donut and spline charts added

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477737286d71938187c702d0f64145d4c2b4fa96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe27442160dcac649de27b568b4854483fb687e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
