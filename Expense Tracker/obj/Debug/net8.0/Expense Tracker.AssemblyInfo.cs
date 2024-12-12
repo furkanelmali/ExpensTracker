964468e0160edb616cadb6f64df04a02585d254b
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2024-12-12 21:58:03 +0300

    v2.5
    Sidebar menu added

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f396039d424f9784c2b9e2e262c614510c7accb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c985a3fbb074d189e022915bdb536b83ca8c2c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
